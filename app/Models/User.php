fc958f1735a682aec2b9fd396061526cb20dda17
mraficaturw <raficaturbusiness@gmail.com>
2026-01-10 13:00:16 +0700

    Fix admin panel access

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return true;
+        return str_ends_with($this->email, '@sikarir.unsika.ac.id');
     }
 }
